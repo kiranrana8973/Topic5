b6694524246df419bc02be497283a9c4d5beacd7
kiran <kiranrana8973@gmail.com>
2019-04-14 17:15:12 +0545

    Finished RecyclerView

---
+++ app/src/main/java/com/example/topic5/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.topic5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import adapter.ContactsAdapter;
+import de.hdodenhof.circleimageview.CircleImageView;
+import model.Contacts;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+
+//    private TextView tvName,tvPhoneNo;
+//    private CircleImageView imgProfile;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        List<Contacts> contactsList = new ArrayList<>();
+
+        contactsList.add( new Contacts("Saugat Malla","908011122",R.drawable.saugat));
+        contactsList.add( new Contacts("Rajesh Hamal","908092212",R.drawable.rajesh));
+        contactsList.add( new Contacts("Dahayang Rai","908092110",R.drawable.dahayang));
+        contactsList.add( new Contacts("Bhuwan KC","908092211",R.drawable.bhuwan));
+
+
+        ContactsAdapter contactsAdapter = new ContactsAdapter(this,contactsList);
+        recyclerView.setAdapter(contactsAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+    }
+}
